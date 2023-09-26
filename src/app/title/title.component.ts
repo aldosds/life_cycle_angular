cb36fe50402866cf74b506fc300eccaff563f7f7
Aldo Santos <aldoacesso@gmail.com>
2023-09-26 14:01:09 -0300

    Criado componente check-sample com os 8 Hooks de LifeCycle Hooks

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  nome:string = 'Aldo'
+  @Input() nome:string = ''
 
-  constructor() { }
+  constructor() {
+    console.log(`Construtor ${this.nome}`);
+  }
+  ngOnChanges(): void {
+    console.log(`OnChange ${this.nome}`);
+  }
 
   ngOnInit(): void {
-    this.nome = `Olá ${this.nome}`
+    this.nome = this.nome + "x"
+    console.log(`OnInit ${this.nome}`);
   }
 
 }
